5221996540a1564f956a43678537171399c73f91
k15122131474 <2591313550@qq.com>
2018-09-26 14:35:40 +0800

    ldinfo

---
+++ SpaceFinanceAnalysis/src/main/java/com/sfa/model/LoanDetailsInfo.java
@@ -0,0 +1,55 @@
+package com.sfa.model;
+
+public class LoanDetailsInfo {
+	private String incompany;
+	private String outcompany;
+	private String starttime;
+	private String endtime;
+	private String lixi;
+	private String weiyue;
+	private String num;
+	
+	public String getIncompany() {
+		return incompany;
+	}
+	public void setIncompany(String incompany) {
+		this.incompany = incompany;
+	}
+	public String getOutcompany() {
+		return outcompany;
+	}
+	public void setOutcompany(String outcompany) {
+		this.outcompany = outcompany;
+	}
+	public String getStarttime() {
+		return starttime;
+	}
+	public void setStarttime(String starttime) {
+		this.starttime = starttime;
+	}
+	public String getEndtime() {
+		return endtime;
+	}
+	public void setEndtime(String endtime) {
+		this.endtime = endtime;
+	}
+	public String getLixi() {
+		return lixi;
+	}
+	public void setLixi(String lixi) {
+		this.lixi = lixi;
+	}
+	public String getWeiyue() {
+		return weiyue;
+	}
+	public void setWeiyue(String weiyue) {
+		this.weiyue = weiyue;
+	}
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	
+}
